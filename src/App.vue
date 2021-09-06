8d490981bcc63b5928053f7495e0d2aeed7d60f3
Ulaş Can Demirbağ <ulascandemirbag@gmail.com>
2021-09-06 09:04:38 +0300

    proje tasarımı tamamlandı

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,114 @@
-<template> </template>
+<template>
+  <div class="container">
+    <div class="form-group mb-4">
+      <select name="" id="" class="form-control">
+        <option>Global</option>
+      </select>
+    </div>
+    <div class="row justify-content-around">
+      <div class="col-md-4">
+        <div class="stats-item btn-danger blue-line">
+          <span class="item-title">
+            <span>Confirmed</span>
+            <i class="fas fa-virus"></i>
+          </span>
+          <span class="item-count">-</span>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="stats-item btn-danger red-line">
+          <span class="item-title"><span>Deaths</span>
+            <i class="fas fa-bed"></i>
+          </span>
+          <span class="item-count">-</span>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="stats-item btn-danger green-line">
+          <span class="item-title">
+            <span>Recovered</span>
+            <i class="fas fa-child"></i>
+          </span>
+          <span class="item-count">-</span>
+        </div>
+      </div>
+    </div>
+    <hr />
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Country</th>
+          <th scope="col">Confirmed</th>
+          <th scope="col">Deaths</th>
+          <th scope="col">Recovered</th>
+        </tr>
+      </thead>
+      <tr>
+        <th scope="row">1</th>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+      </tr>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style>
+body {
+  background: antiquewhite;
+}
+table {
+  background: #fff;
+}
+.container {
+  margin-top: 40px;
+}
+.stats-item {
+  padding: 20px;
+  text-align: center;
+  margin-bottom: 20px;
+  color: rgb(49, 49, 49);
+  border-radius: 15px 0px 0px 15px;
+  background-color: #fff;
+}
+.green-line{
+  border-left: 8px solid rgb(42, 221, 111);
+}
+.green-line:hover{
+  background-color:  rgb(42, 221, 111);
+  border-left: 8px solid  rgb(49, 49, 49);
+}
+.blue-line{
+  border-left: 8px solid rgb(42, 105, 221);
+}
+.blue-line:hover{
+  background-color: rgb(42, 105, 221);
+  border-left: 8px solid  rgb(49, 49, 49);
+}
+.red-line{
+  border-left: 8px solid rgb(226, 59, 59);
+}
+.red-line:hover{
+  background-color:  rgb(226, 59, 59);
+  border-left: 8px solid  rgb(49, 49, 49);
+}
+.item-title {
+  display: block;
+  font-size: 25px;
+  display: flex;
+  justify-content: space-around;
+}
+.item-title i {
+  font-size: 40px;
+}
+.item-count {
+  font-weight: bold;
+  font-size: 30px;
+}
+</style>
